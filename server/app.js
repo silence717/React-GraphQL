dea82953f290ffb7c56206d255c741b0f5434ad0
fang.yang <yangfang2028@126.com>
2021-04-12 22:28:53 +0800

    ✨ install graphql express-graphql

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
 const port = 1234;
+
+// app.use('/graphql', graphqlHTTP({
+
+// }));
+
+app.use('/graphql', graphqlHTTP({
+    // pass in a schema property
+}));
 
 app.listen(port, () => {
     console.log(`listening requests on port ${port}`);
